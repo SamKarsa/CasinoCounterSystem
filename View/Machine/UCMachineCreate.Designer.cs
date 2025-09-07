f3f9bc949190e017331b4c95acf0e9327ebafe65
SamKarsa <samuellopezmarin2006@gmail.com>
2025-09-06 20:39:37 -0500

    Created Machine and Route User Controls

---
+++ View/Machine/UCMachineCreate.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace CasinoCounterSystem.View.Machine
+{
+    partial class UCMachineCreate
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            imgLogo = new PictureBox();
+            labelTitle = new Sunny.UI.UILabel();
+            textBoxNumMachine = new Sunny.UI.UITextBox();
+            this.comboBoxRoute = new Sunny.UI.UIComboBox();
+            comboBoxMachineType = new Sunny.UI.UIComboBox();
+            comboBoxCoinType = new Sunny.UI.UIComboBox();
+            uiLabel1 = new Sunny.UI.UILabel();
+            textBoxNameClient = new Sunny.UI.UITextBox();
+            textBoxPhone = new Sunny.UI.UITextBox();
+            textBoxAddress = new Sunny.UI.UITextBox();
+            btnCancelRoute = new Sunny.UI.UIButton();
+            btnSaveRoute = new Sunny.UI.UIButton();
+            ((System.ComponentModel.ISupportInitialize)imgLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // imgLogo
+            // 
+            imgLogo.BackColor = Color.Transparent;
+            imgLogo.Image = Properties.Resources.logoCltElectronic;
+            imgLogo.Location = new Point(764, 620);
+            imgLogo.Name = "imgLogo";
+            imgLogo.Size = new Size(89, 86);
+            imgLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgLogo.TabIndex = 15;
+            imgLogo.TabStop = false;
+            // 
+            // labelTitle
+            // 
+            labelTitle.Font = new Font("Britannic Bold", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelTitle.ForeColor = Color.Navy;
+            labelTitle.Location = new Point(272, 90);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(354, 50);
+            labelTitle.TabIndex = 16;
+            labelTitle.Text = "Create Machine";
+            // 
+            // textBoxNumMachine
+            // 
+            textBoxNumMachine.ButtonRectColor = Color.Navy;
+            textBoxNumMachine.ButtonRectHoverColor = Color.FromArgb(30, 58, 138);
+            textBoxNumMachine.ButtonRectPressColor = Color.FromArgb(37, 99, 235);
+            textBoxNumMachine.ButtonStyleInherited = false;
+            textBoxNumMachine.Font = new Font("Microsoft Sans Serif", 12F);
+            textBoxNumMachine.Location = new Point(428, 225);
+            textBoxNumMachine.Margin = new Padding(4, 5, 4, 5);
+            textBoxNumMachine.MinimumSize = new Size(1, 16);
+            textBoxNumMachine.Name = "textBoxNumMachine";
+            textBoxNumMachine.Padding = new Padding(12, 5, 12, 5);
+            textBoxNumMachine.Radius = 6;
+            textBoxNumMachine.RectColor = Color.Navy;
+            textBoxNumMachine.RectSize = 2;
+            textBoxNumMachine.ShowText = false;
+            textBoxNumMachine.Size = new Size(346, 50);
+            textBoxNumMachine.TabIndex = 18;
+            textBoxNumMachine.TextAlignment = ContentAlignment.MiddleLeft;
+            textBoxNumMachine.Watermark = "Enter Number Machine";
+            textBoxNumMachine.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // comboBoxRoute
+            // 
+            this.comboBoxRoute.DataSource = null;
+            this.comboBoxRoute.FillColor = Color.White;
+            this.comboBoxRoute.Font = new Font("Microsoft Sans Serif", 12F);
+            this.comboBoxRoute.ItemHoverColor = Color.FromArgb(30, 58, 138);
+            this.comboBoxRoute.ItemSelectForeColor = Color.FromArgb(235, 243, 255);
+            this.comboBoxRoute.Location = new Point(74, 165);
+            this.comboBoxRoute.Margin = new Padding(4, 5, 4, 5);
+            this.comboBoxRoute.MinimumSize = new Size(63, 0);
+            this.comboBoxRoute.Name = "comboBoxRoute";
+            this.comboBoxRoute.Padding = new Padding(0, 0, 30, 2);
+            this.comboBoxRoute.RectColor = Color.Navy;
+            this.comboBoxRoute.Size = new Size(346, 50);
+            this.comboBoxRoute.SymbolSize = 24;
+            this.comboBoxRoute.TabIndex = 23;
+            this.comboBoxRoute.TextAlignment = ContentAlignment.MiddleLeft;
+            this.comboBoxRoute.Watermark = "Select Route";
+            this.comboBoxRoute.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // comboBoxMachineType
+            // 
+            comboBoxMachineType.DataSource = null;
+            comboBoxMachineType.FillColor = Color.White;
+            comboBoxMachineType.Font = new Font("Microsoft Sans Serif", 12F);
+            comboBoxMachineType.ItemHoverColor = Color.FromArgb(30, 58, 138);
+            comboBoxMachineType.ItemSelectForeColor = Color.FromArgb(235, 243, 255);
+            comboBoxMachineType.Location = new Point(74, 225);
+            comboBoxMachineType.Margin = new Padding(4, 5, 4, 5);
+            comboBoxMachineType.MinimumSize = new Size(63, 0);
+            comboBoxMachineType.Name = "comboBoxMachineType";
+            comboBoxMachineType.Padding = new Padding(0, 0, 30, 2);
+            comboBoxMachineType.RectColor = Color.Navy;
+            comboBoxMachineType.Size = new Size(346, 50);
+            comboBoxMachineType.SymbolSize = 24;
+            comboBoxMachineType.TabIndex = 24;
+            comboBoxMachineType.TextAlignment = ContentAlignment.MiddleLeft;
+            comboBoxMachineType.Watermark = "Select Machine Type";
+            comboBoxMachineType.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // comboBoxCoinType
+            // 
+            comboBoxCoinType.DataSource = null;
+            comboBoxCoinType.FillColor = Color.White;
+            comboBoxCoinType.Font = new Font("Microsoft Sans Serif", 12F);
+            comboBoxCoinType.ItemHoverColor = Color.FromArgb(30, 58, 138);
+            comboBoxCoinType.ItemSelectForeColor = Color.FromArgb(235, 243, 255);
+            comboBoxCoinType.Location = new Point(428, 165);
+            comboBoxCoinType.Margin = new Padding(4, 5, 4, 5);
+            comboBoxCoinType.MinimumSize = new Size(63, 0);
+            comboBoxCoinType.Name = "comboBoxCoinType";
+            comboBoxCoinType.Padding = new Padding(0, 0, 30, 2);
+            comboBoxCoinType.RectColor = Color.Navy;
+            comboBoxCoinType.Size = new Size(346, 50);
+            comboBoxCoinType.SymbolSize = 24;
+            comboBoxCoinType.TabIndex = 25;
+            comboBoxCoinType.TextAlignment = ContentAlignment.MiddleLeft;
+            comboBoxCoinType.Watermark = "Select Coin Type";
+            comboBoxCoinType.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // uiLabel1
+            // 
+            uiLabel1.Font = new Font("Britannic Bold", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            uiLabel1.ForeColor = Color.Navy;
+            uiLabel1.Location = new Point(301, 318);
+            uiLabel1.Name = "uiLabel1";
+            uiLabel1.Size = new Size(297, 50);
+            uiLabel1.TabIndex = 26;
+            uiLabel1.Text = "Info Machine";
+            // 
+            // textBoxNameClient
+            // 
+            textBoxNameClient.ButtonRectColor = Color.Navy;
+            textBoxNameClient.ButtonRectHoverColor = Color.FromArgb(30, 58, 138);
+            textBoxNameClient.ButtonRectPressColor = Color.FromArgb(37, 99, 235);
+            textBoxNameClient.ButtonStyleInherited = false;
+            textBoxNameClient.Font = new Font("Microsoft Sans Serif", 12F);
+            textBoxNameClient.Location = new Point(71, 396);
+            textBoxNameClient.Margin = new Padding(4, 5, 4, 5);
+            textBoxNameClient.MinimumSize = new Size(1, 16);
+            textBoxNameClient.Name = "textBoxNameClient";
+            textBoxNameClient.Padding = new Padding(12, 5, 12, 5);
+            textBoxNameClient.Radius = 6;
+            textBoxNameClient.RectColor = Color.Navy;
+            textBoxNameClient.RectSize = 2;
+            textBoxNameClient.ShowText = false;
+            textBoxNameClient.Size = new Size(346, 50);
+            textBoxNameClient.TabIndex = 19;
+            textBoxNameClient.TextAlignment = ContentAlignment.MiddleLeft;
+            textBoxNameClient.Watermark = "Enter Name Client";
+            textBoxNameClient.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // textBoxPhone
+            // 
+            textBoxPhone.ButtonRectColor = Color.Navy;
+            textBoxPhone.ButtonRectHoverColor = Color.FromArgb(30, 58, 138);
+            textBoxPhone.ButtonRectPressColor = Color.FromArgb(37, 99, 235);
+            textBoxPhone.ButtonStyleInherited = false;
+            textBoxPhone.Font = new Font("Microsoft Sans Serif", 12F);
+            textBoxPhone.Location = new Point(425, 396);
+            textBoxPhone.Margin = new Padding(4, 5, 4, 5);
+            textBoxPhone.MinimumSize = new Size(1, 16);
+            textBoxPhone.Name = "textBoxPhone";
+            textBoxPhone.Padding = new Padding(12, 5, 12, 5);
+            textBoxPhone.Radius = 6;
+            textBoxPhone.RectColor = Color.Navy;
+            textBoxPhone.RectSize = 2;
+            textBoxPhone.ShowText = false;
+            textBoxPhone.Size = new Size(346, 50);
+            textBoxPhone.TabIndex = 20;
+            textBoxPhone.TextAlignment = ContentAlignment.MiddleLeft;
+            textBoxPhone.Watermark = "Enter Phone Client";
+            textBoxPhone.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // textBoxAddress
+            // 
+            textBoxAddress.ButtonRectColor = Color.Navy;
+            textBoxAddress.ButtonRectHoverColor = Color.FromArgb(30, 58, 138);
+            textBoxAddress.ButtonRectPressColor = Color.FromArgb(37, 99, 235);
+            textBoxAddress.ButtonStyleInherited = false;
+            textBoxAddress.Font = new Font("Microsoft Sans Serif", 12F);
+            textBoxAddress.Location = new Point(277, 456);
+            textBoxAddress.Margin = new Padding(4, 5, 4, 5);
+            textBoxAddress.MinimumSize = new Size(1, 16);
+            textBoxAddress.Name = "textBoxAddress";
+            textBoxAddress.Padding = new Padding(12, 5, 12, 5);
+            textBoxAddress.Radius = 6;
+            textBoxAddress.RectColor = Color.Navy;
+            textBoxAddress.RectSize = 2;
+            textBoxAddress.ShowText = false;
+            textBoxAddress.Size = new Size(346, 50);
+            textBoxAddress.TabIndex = 21;
+            textBoxAddress.TextAlignment = ContentAlignment.MiddleLeft;
+            textBoxAddress.Watermark = "Enter Address";
+            textBoxAddress.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // btnCancelRoute
+            // 
+            btnCancelRoute.BackColor = Color.White;
+            btnCancelRoute.FillColor = Color.FromArgb(229, 231, 235);
+            btnCancelRoute.FillColor2 = Color.FromArgb(229, 231, 235);
+            btnCancelRoute.FillDisableColor = Color.FromArgb(243, 244, 246);
+            btnCancelRoute.FillHoverColor = Color.FromArgb(209, 213, 219);
+            btnCancelRoute.FillPressColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.FillSelectedColor = Color.FromArgb(209, 213, 219);
+            btnCancelRoute.Font = new Font("Microsoft Sans Serif", 12F);
+            btnCancelRoute.ForeColor = Color.Navy;
+            btnCancelRoute.ForeDisableColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.ForeHoverColor = Color.Navy;
+            btnCancelRoute.ForePressColor = Color.Navy;
+            btnCancelRoute.ForeSelectedColor = Color.Navy;
+            btnCancelRoute.Location = new Point(475, 559);
+            btnCancelRoute.MinimumSize = new Size(1, 1);
+            btnCancelRoute.Name = "btnCancelRoute";
+            btnCancelRoute.RectColor = Color.FromArgb(209, 213, 219);
+            btnCancelRoute.RectDisableColor = Color.FromArgb(229, 231, 235);
+            btnCancelRoute.RectHoverColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.RectPressColor = Color.FromArgb(107, 114, 128);
+            btnCancelRoute.RectSelectedColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.Size = new Size(155, 50);
+            btnCancelRoute.TabIndex = 28;
+            btnCancelRoute.Text = "🗑️ Cancel";
+            btnCancelRoute.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnSaveRoute
+            // 
+            btnSaveRoute.BackColor = Color.White;
+            btnSaveRoute.FillColor = Color.Navy;
+            btnSaveRoute.FillColor2 = Color.Navy;
+            btnSaveRoute.FillDisableColor = Color.FromArgb(229, 231, 235);
+            btnSaveRoute.FillHoverColor = Color.FromArgb(30, 58, 138);
+            btnSaveRoute.FillPressColor = Color.FromArgb(0, 0, 102);
+            btnSaveRoute.FillSelectedColor = Color.FromArgb(30, 64, 175);
+            btnSaveRoute.Font = new Font("Microsoft Sans Serif", 12F);
+            btnSaveRoute.ForeDisableColor = Color.FromArgb(156, 163, 175);
+            btnSaveRoute.Location = new Point(262, 559);
+            btnSaveRoute.MinimumSize = new Size(1, 1);
+            btnSaveRoute.Name = "btnSaveRoute";
+            btnSaveRoute.RectColor = Color.Navy;
+            btnSaveRoute.RectDisableColor = Color.FromArgb(209, 213, 219);
+            btnSaveRoute.RectHoverColor = Color.FromArgb(30, 58, 138);
+            btnSaveRoute.RectPressColor = Color.FromArgb(0, 0, 102);
+            btnSaveRoute.RectSelectedColor = Color.FromArgb(30, 64, 175);
+            btnSaveRoute.Size = new Size(155, 50);
+            btnSaveRoute.TabIndex = 27;
+            btnSaveRoute.Text = "➕ Save";
+            btnSaveRoute.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            // 
+            // UCMachineCreate
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(btnCancelRoute);
+            Controls.Add(btnSaveRoute);
+            Controls.Add(textBoxAddress);
+            Controls.Add(textBoxPhone);
+            Controls.Add(textBoxNameClient);
+            Controls.Add(uiLabel1);
+            Controls.Add(comboBoxCoinType);
+            Controls.Add(comboBoxMachineType);
+            Controls.Add(this.comboBoxRoute);
+            Controls.Add(textBoxNumMachine);
+            Controls.Add(labelTitle);
+            Controls.Add(imgLogo);
+            Name = "UCMachineCreate";
+            Size = new Size(863, 712);
+            ((System.ComponentModel.ISupportInitialize)imgLogo).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox imgLogo;
+        private Sunny.UI.UILabel labelTitle;
+        private Sunny.UI.UITextBox textBoxNumMachine;
+        private Sunny.UI.UITextBox textBoxCoin;
+        private Sunny.UI.UITextBox uiTextBox3;
+        private Sunny.UI.UIComboBox uiComboBox2;
+        private Sunny.UI.UIComboBox ComboBoxRoute;
+        private Sunny.UI.UIComboBox comboBoxMachineType;
+        private Sunny.UI.UIComboBox comboBoxCoinType;
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UITextBox textBoxNameClient;
+        private Sunny.UI.UITextBox textBoxPhone;
+        private Sunny.UI.UITextBox textBoxAddress;
+        private Sunny.UI.UIButton btnCancelRoute;
+        private Sunny.UI.UIButton btnSaveRoute;
+    }
+}
---
+++ View/Route/UCRouteCreate.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace CasinoCounterSystem.View.Route
+{
+    partial class UCRouteCreate
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            imgLogo = new PictureBox();
+            labelTitle = new Sunny.UI.UILabel();
+            textBoxRoute = new Sunny.UI.UITextBox();
+            btnSaveRoute = new Sunny.UI.UIButton();
+            btnCancelRoute = new Sunny.UI.UIButton();
+            ((System.ComponentModel.ISupportInitialize)imgLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // imgLogo
+            // 
+            imgLogo.BackColor = Color.Transparent;
+            imgLogo.Image = Properties.Resources.logoCltElectronic;
+            imgLogo.Location = new Point(764, 620);
+            imgLogo.Name = "imgLogo";
+            imgLogo.Size = new Size(89, 86);
+            imgLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgLogo.TabIndex = 14;
+            imgLogo.TabStop = false;
+            // 
+            // labelTitle
+            // 
+            labelTitle.Font = new Font("Britannic Bold", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelTitle.ForeColor = Color.Navy;
+            labelTitle.Location = new Point(273, 197);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(302, 50);
+            labelTitle.TabIndex = 15;
+            labelTitle.Text = "Create Route";
+            // 
+            // textBoxRoute
+            // 
+            textBoxRoute.ButtonRectColor = Color.Navy;
+            textBoxRoute.ButtonRectHoverColor = Color.FromArgb(30, 58, 138);
+            textBoxRoute.ButtonRectPressColor = Color.FromArgb(37, 99, 235);
+            textBoxRoute.ButtonStyleInherited = false;
+            textBoxRoute.Font = new Font("Microsoft Sans Serif", 12F);
+            textBoxRoute.Location = new Point(237, 261);
+            textBoxRoute.Margin = new Padding(4, 5, 4, 5);
+            textBoxRoute.MinimumSize = new Size(1, 16);
+            textBoxRoute.Name = "textBoxRoute";
+            textBoxRoute.Padding = new Padding(12, 5, 12, 5);
+            textBoxRoute.Radius = 6;
+            textBoxRoute.RectColor = Color.Navy;
+            textBoxRoute.RectSize = 2;
+            textBoxRoute.ShowText = false;
+            textBoxRoute.Size = new Size(368, 71);
+            textBoxRoute.TabIndex = 16;
+            textBoxRoute.TextAlignment = ContentAlignment.MiddleLeft;
+            textBoxRoute.Watermark = "Enter route name (e.g., Route A)";
+            textBoxRoute.WatermarkActiveColor = Color.DarkGray;
+            // 
+            // btnSaveRoute
+            // 
+            btnSaveRoute.BackColor = Color.White;
+            btnSaveRoute.FillColor = Color.Navy;
+            btnSaveRoute.FillColor2 = Color.Navy;
+            btnSaveRoute.FillDisableColor = Color.FromArgb(229, 231, 235);
+            btnSaveRoute.FillHoverColor = Color.FromArgb(30, 58, 138);
+            btnSaveRoute.FillPressColor = Color.FromArgb(0, 0, 102);
+            btnSaveRoute.FillSelectedColor = Color.FromArgb(30, 64, 175);
+            btnSaveRoute.Font = new Font("Microsoft Sans Serif", 12F);
+            btnSaveRoute.ForeDisableColor = Color.FromArgb(156, 163, 175);
+            btnSaveRoute.Location = new Point(237, 358);
+            btnSaveRoute.MinimumSize = new Size(1, 1);
+            btnSaveRoute.Name = "btnSaveRoute";
+            btnSaveRoute.RectColor = Color.Navy;
+            btnSaveRoute.RectDisableColor = Color.FromArgb(209, 213, 219);
+            btnSaveRoute.RectHoverColor = Color.FromArgb(30, 58, 138);
+            btnSaveRoute.RectPressColor = Color.FromArgb(0, 0, 102);
+            btnSaveRoute.RectSelectedColor = Color.FromArgb(30, 64, 175);
+            btnSaveRoute.Size = new Size(155, 50);
+            btnSaveRoute.TabIndex = 17;
+            btnSaveRoute.Text = "➕ Save";
+            btnSaveRoute.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnCancelRoute
+            // 
+            btnCancelRoute.BackColor = Color.White;
+            btnCancelRoute.FillColor = Color.FromArgb(229, 231, 235);
+            btnCancelRoute.FillColor2 = Color.FromArgb(229, 231, 235);
+            btnCancelRoute.FillDisableColor = Color.FromArgb(243, 244, 246);
+            btnCancelRoute.FillHoverColor = Color.FromArgb(209, 213, 219);
+            btnCancelRoute.FillPressColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.FillSelectedColor = Color.FromArgb(209, 213, 219);
+            btnCancelRoute.Font = new Font("Microsoft Sans Serif", 12F);
+            btnCancelRoute.ForeColor = Color.Navy;
+            btnCancelRoute.ForeDisableColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.ForeHoverColor = Color.Navy;
+            btnCancelRoute.ForePressColor = Color.Navy;
+            btnCancelRoute.ForeSelectedColor = Color.Navy;
+            btnCancelRoute.Location = new Point(450, 358);
+            btnCancelRoute.MinimumSize = new Size(1, 1);
+            btnCancelRoute.Name = "btnCancelRoute";
+            btnCancelRoute.RectColor = Color.FromArgb(209, 213, 219);
+            btnCancelRoute.RectDisableColor = Color.FromArgb(229, 231, 235);
+            btnCancelRoute.RectHoverColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.RectPressColor = Color.FromArgb(107, 114, 128);
+            btnCancelRoute.RectSelectedColor = Color.FromArgb(156, 163, 175);
+            btnCancelRoute.Size = new Size(155, 50);
+            btnCancelRoute.TabIndex = 18;
+            btnCancelRoute.Text = "🗑️ Cancel";
+            btnCancelRoute.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            // 
+            // UCRouteCreate
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(btnCancelRoute);
+            Controls.Add(btnSaveRoute);
+            Controls.Add(textBoxRoute);
+            Controls.Add(labelTitle);
+            Controls.Add(imgLogo);
+            Name = "UCRouteCreate";
+            Size = new Size(863, 712);
+            ((System.ComponentModel.ISupportInitialize)imgLogo).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox imgLogo;
+        private Sunny.UI.UILabel labelTitle;
+        private Sunny.UI.UITextBox textBoxRoute;
+        private Sunny.UI.UIButton btnSaveRoute;
+        private Sunny.UI.UIButton btnCancelRoute;
+    }
+}
